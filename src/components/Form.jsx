9daa7de89a3e353f532e2c17649a4fced8b3787c
juoliva95 <juoliva92@users.noreply.github.com>
2022-11-01 23:52:53 -0300

    State con objetos cripto generados consumiendo API

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import { monedas } from "../data/monedas";
@@ -23,16 +23,26 @@ const InputSubmit = styled.input`
 `;
 
 const Form = () => {
+  const [criptos, setCriptos] = useState([]);
   const [moneda, SelectMonedas] = useSelectMonedas("Elige una moneda", monedas);
 
   useEffect(() => {
     const consultarAPI = async () => {
-        const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
 
-        const respuesta = await fetch(url);
-        const resultado = await respuesta.json();
-        console.log(resultado.Data)
-    }
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      const arrayCriptos = resultado.Data.map((cripto) => {
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName,
+        };
+        return objeto;
+      });
+      setCriptos(arrayCriptos);
+    };
     consultarAPI();
   }, []);
 
