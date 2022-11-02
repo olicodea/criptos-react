f29569872c5935a27a8ef46a940ee99b79e14728
juoliva95 <juoliva92@users.noreply.github.com>
2022-11-01 23:44:11 -0300

    Formular y styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-import { useState } from 'react'
+import { useState } from "react";
+import styled from "@emotion/styled";
+import Form from "./components/Form";
+import ImagenCripto from "./img/imagen-criptos.png";
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
+const Heading = styled.h1`
+  font-family: "Gantari", sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>APP</h1>
-  )
+    <Contenedor>
+      <Imagen src={ImagenCripto} alt="imagenes cripto"></Imagen>
+      <div>
+        <Heading>Cotiza Criptomonedas en el acto!</Heading>
+        <Form />
+      </div>
+    </Contenedor>
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+  { id: "USD", nombre: "Dolar estadounidense" },
+  { id: "MXN", nombre: "Peso mexicano" },
+  { id: "EUR", nombre: "Euro" },
+  { id: "ARS", nombre: "Peso argentino" },
+];
+
+export { monedas };
